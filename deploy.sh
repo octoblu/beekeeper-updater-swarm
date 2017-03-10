9d4a974b1a24b4be2f93a968e2427ae147e943ac
Jade Meskill <jade.meskill@gmail.com>
2017-03-10 14:04:46 -0700

    v2.1.6 try autobuild

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 APP_NAME=beekeeper-updater-swarm
-LOCAL_IMAGE_NAME=local/$APP_NAME
-REMOTE_IMAGE_NAME=octoblu/$APP_NAME
+LOCAL_IMAGE_NAME="local/$APP_NAME"
+REMOTE_IMAGE_NAME="octoblu/$APP_NAME"
 
-panic() {
+fatal() {
   local message=$1
-  echo $message
+  echo "$message"
   exit 1
 }
 
 main() {
-  local tag="$(git describe --tags --exact --match 'v*.*.*')"
-  if [ -n "$BUILD_TAG" ]; then
-    echo "Using $BUILD_TAG tag"
-    tag="$BUILD_TAG"
+  echo "#######################################################################"
+  echo "# WARNING: You may now gump and codefresh will deploy automagically!! #"
+  echo "#######################################################################"
+  echo 
+  read -p "Are you sure you want to manually deploy? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    local tag="$1"
+    if [ -z "$tag" ]; then
+      fatal "Missing tag as first argument"
+    fi
+    echo "building $LOCAL_IMAGE_NAME:$tag"
+    ./build.sh "$tag" || fatal 'build failed'
+    echo "tagging $LOCAL_IMAGE_NAME:$tag -> $REMOTE_IMAGE_NAME:$tag"
+    docker tag "$LOCAL_IMAGE_NAME:$tag" "$REMOTE_IMAGE_NAME:$tag" || fatal 'failed to tag'
+    echo "pushing $REMOTE_IMAGE_NAME:$tag"
+    docker push "$REMOTE_IMAGE_NAME:$tag" || fatal 'failed to push'
   fi
-  if [ "$?" != "0" ]; then
-    panic 'not a tagged commit'
-  fi
-  ./build.sh || panic 'build failed'
-  echo "building tag $tag"
-  docker tag $LOCAL_IMAGE_NAME:latest $REMOTE_IMAGE_NAME:$tag
-  docker push $REMOTE_IMAGE_NAME:$tag
 }
 
-main
+main "$@"
